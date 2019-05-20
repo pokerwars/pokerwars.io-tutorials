7cb6646ecc03c5607aac7917d50c19433e40dd46
Giulio Leso <giulio.leso@transferwise.com>
2019-05-20 10:27:53 +0100

    first 2 steps for python tutorial

---
+++ python/step_2.py
@@ -0,0 +1,17 @@
+//imports
+
+from threading import Thread
+from time      import sleep
+from sys       import exit
+from bottle    import get, post, run, request
+
+import requests
+
+port          = 8090
+
+@post('/pokerwars.io/notifications')
+
+// Add here the 2 missing endpoint
+
+
+run(host='0.0.0.0', port=port)
