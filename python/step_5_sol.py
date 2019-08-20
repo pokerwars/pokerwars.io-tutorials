844d974e0c147486a7ed6dbeb28461825f4722ef
giulio.leso <giulio.leso@transferwise.com>
2019-08-20 17:04:04 +0100

    breaking down the steps and adding new steps

--- python/step_5_sol.py
+++ python/step_5_sol.py
@@ -4,9 +4,6 @@ from threading import Thread
 from time      import sleep
 from dotenv    import load_dotenv
 from sys       import exit
-
-// Add the missing import here
-
 from bottle    import get, post, run, request
 
 import requests
@@ -15,5 +12,15 @@ import os
 load_dotenv()
 
 port          = 3000
+
+@post('/pokerwars.io/notifications')
+
+@get('/pokerwars.io/ping')
+def ping():
+    print('Received ping from pokerwars.io, responding with a pong')
+    response.content_type = 'application/json'
+    return {"pong": True}
+
+@post('/pokerwars.io/play')
 
 run(host='0.0.0.0', port=port)
